7031524a4ea6c23b0fdcb1ed2597684e95f57ddb
Carson Rivera <carsonrivera04@gmail.com>
2025-11-03 15:46:21 -0500

    Created separate dtb file and removed globl line from program_1.asm

---
+++ dtb.asm
@@ -0,0 +1,44 @@
+.data
+arr: .space 400
+.text
+main: 
+    li $s1, 387       # decimal 
+    la $s0, arr     # base address of empty array 
+    li $t0, 0       # loop counter 
+for:
+    sll $t1,$t0,2         # $t1=i*4 address of index 
+    add $t1,$t1,$s0   # $t1 = arr + i * 4
+
+
+    li $t9, 2 
+    div $s1, $t9  
+    mflo $s1
+    mfhi $t7 
+
+    sw $t7,0($t1)      # $t7 = arr[i]
+    addi $t0,$t0,1     # increment loop counter i
+    bne $s1, $zero, for
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~
+    la $s0,arr             # store base address of array in $s0
+    addi $t0, $t0, -1 
+    li $t3, -1 
+print:
+    beq $t0,$t3,end
+    sll $t1,$t0,2       # array traversal to access arr[i]
+    add $t1,$t1,$s0
+    lw $t7,0($t1)       # $t7=arr[i]
+    li $v0,1            # 1 is the syscall number to print integers
+    move $a0,$t7         # print $t7
+    syscall
+
+    li $a0, 32     # 32 is the ASCII code for space
+    li $v0, 11          # syscall number for printing character
+    syscall
+    addi $t0,$t0,-1 # increment loop counter
+    j print
+end:
+
+    # exit program 
+    li $v0, 10      # Exit system call  
+    syscall
